cf9def20e8fd67383bec47ed33499c9214abb706
Ayub Nasir <119772377+Ayubish@users.noreply.github.com>
2025-01-09 21:21:06 +0300

    added some js functionalities

--- main.js
+++ main.js
@@ -2,13 +2,22 @@ const popup = document.querySelector(".add-popup");
 const popupBox = document.querySelector(".popup-box");
 const notesContainer = document.querySelector(".notes");
 
-const notes = [{ note: "Wancky note 1" }, { note: "Dancky note 100" }];
+let notes = [];
+
 const togglePopUp = () => {
   popup.classList.toggle("d-none");
   popupBox.classList.toggle("d-anima");
   popupBox.classList.toggle("d-no-anima");
 };
+function parseNotes() {
+  if (localStorage.notes) {
+    notes = JSON.parse(localStorage.notes);
+  } else {
+    localStorage.notes = JSON.stringify(notes);
+  }
+}
 function renderNotes() {
+  parseNotes();
   if (notes.length == 0) {
     const h2 = document.createElement("h2");
     h2.innerText = "Nothing to see...";
@@ -20,12 +29,14 @@ function renderNotes() {
       const div = document.createElement("div");
       div.classList.add("note");
       div.innerHTML = `<span>
-      <input type="checkbox" />
-      <p>${note.note}</p>
+      <input type="checkbox" onchange="${() => tickCompleted(index)}"/> 
+      <p class=${notes[index].isCompleted ? "completed" : "none"}>${
+        note.note
+      }</p>
     </span>
     <span>
-      <p>edit</p>
-      <p>delete</p>
+      <p onclick=(editNote(${index}))>edit</p>
+      <p onclick=(deleteNote(${index}))>delete</p>
       </span>`;
       notesContainer.appendChild(div);
     });
@@ -35,8 +46,29 @@ renderNotes();
 const record = () => {
   const noteInput = document.getElementById("note-input").value;
   document.getElementById("note-input").value = "";
-  notes.push({ note: noteInput });
-  console.log(notes);
+  parseNotes();
+  notes.push({ note: noteInput, isCompleted: false });
+  //notes.push({ note: noteInput });
+  localStorage.notes = JSON.stringify(notes);
+
   togglePopUp();
   renderNotes();
 };
+
+function tickCompleted(index) {
+  parseNotes();
+  notes[index].isCompleted = !notes[index].isCompleted;
+  localStorage.notes = JSON.stringify(notes);
+}
+function editNote(index) {
+  parseNotes();
+  togglePopUp();
+  document.getElementById("note-input").value = notes[index].note;
+  deleteNote(index);
+}
+function deleteNote(index) {
+  parseNotes();
+  notes.splice(index, 1);
+  localStorage.notes = JSON.stringify(notes);
+  renderNotes();
+}
